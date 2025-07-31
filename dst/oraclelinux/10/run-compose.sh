002dd709720073cc755b638cb2e324a9625c7f3d
yasu0796 <yasu0796@msn.com>
2025-07-31 22:43:51 +0900

    Update for Oracle Linux

--- dst/oraclelinux/10/run-compose.sh
+++ dst/oraclelinux/10/run-compose.sh
@@ -1,3 +1,3 @@
 docker compose up -d
-docker compose exec -it ol8 bash
+docker compose exec -it ol10 bash
 docker compose down
--- dst/oraclelinux/9/run.sh
+++ dst/oraclelinux/9/run.sh
@@ -1,2 +1,3 @@
 docker compose up -d
 docker compose exec -it ol9 bash
+docker compose down
