df6bdf4ce0732707f870c090c6ffc6a94b27a1cf
yasu0796 <20511286+yasu0796@users.noreply.github.com>
2026-07-27 23:13:23 +0900

    Update check_shell for exlude .vscode directory

--- scripts/check_shell.sh
+++ scripts/check_shell.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Run shellcheck on every shell script under the repository tree.
-find .. -type f -name '*.sh' -exec shellcheck {} +
+
+# exclude SC1091 for env file
+find .. \( -name '.vscode' -o -name 'node_modules' \) -prune -o -type f -name '*.sh' -exec shellcheck -e SC1091 {} +
